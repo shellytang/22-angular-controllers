0327de30ad2e14d2ff4e8d58c687202ba6999ed4
Carolina Vasquez-Ceja <cejac10@gmail.com>
2017-02-04 13:22:03 -0800

    changed to controller as syntax

--- lab-carolina/app/entry.js
+++ lab-carolina/app/entry.js
@@ -7,17 +7,17 @@ const angular = require('angular');
 
 const demoApp = angular.module('demoApp', []);
 
-demoApp.controller('CowsayController', ['$log', '$scope', CowsayController]);
+demoApp.controller('CowsayController', ['$log', CowsayController]);
 
-function CowsayController($log, $scope){
-  let cowsayCtrl = $scope.cowsayCtrl = {};
-  cowsayCtrl.title = 'Moooooo';
+function CowsayController($log){
+  let cowsayCtrl = this.cowsayCtrl = {};
+  this.title = 'Moooooo';
 
-  cowsayCtrl.updateCow = function(input){
+  this.updateCow = function(input){
     return '\n' + cowsay.think({text: input || 'What am I thinking about'});
   };
 
-  cowsayCtrl.helloClick = function(input){
+  this.helloClick = function(input){
     $log.log(input);
   };
 
