576808db4e7ca0ea9d57407f29f3ecdae646f896
Carolina Vasquez-Ceja <cejac10@gmail.com>
2017-02-04 13:48:22 -0800

    have select menu working

--- lab-carolina/app/entry.js
+++ lab-carolina/app/entry.js
@@ -10,11 +10,21 @@ const demoApp = angular.module('demoApp', []);
 demoApp.controller('CowsayController', ['$log', CowsayController]);
 
 function CowsayController($log){
-  let cowsayCtrl = this.cowsayCtrl = {};
   this.title = 'Moooooo';
+  this.history = [];
+
+  cowsay.list((err, list) => {
+    this.list = list;
+    this.currentCow = this.list[0];
+  });
 
   this.updateCow = function(input){
     return '\n' + cowsay.think({text: input || 'What am I thinking about'});
+  };
+
+  this.saveText = function(input){
+    this.currText = this.updateCow(input);
+    this.history.push(this.currText);
   };
 
   this.helloClick = function(input){
