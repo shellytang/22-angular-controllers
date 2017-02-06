6687c26fce8d411f9a734c648b372113b7e329da
Rubiksron <rubiksron@gmail.com>
2017-02-05 18:29:39 -0800

    removing comments and console logs, adding css flexbox and color.

--- ron-lab/app/entry.js
+++ ron-lab/app/entry.js
@@ -11,31 +11,28 @@ demoApp.controller('CowsayController', [ '$log', CowsayController]);
 
 function CowsayController($log){
   $log.debug('init cowsayCtrl');
-  this.title = 'Moooooo';
+  this.title = 'Gobble Gobble!';
   this.history = [];
 
   cowsay.list((err, cowfiles) => {
     this.cowfiles = cowfiles;
     this.currentCow = this.cowfiles[0];
-    console.log('this.cowfiles', this.cowfiles);
   });
 
   this.updateCow = function(input){
     $log.debug('this.updateCow()');
-    return '\n' + cowsay.say({text: input || 'gimme something to say', f: this.currentCow});
+    return '\n' + cowsay.say({text: input || 'Gobble!', f: this.currentCow});
   };
 
   this.speak = function(input){
     $log.debug('this.updateCow()');
     this.spoken = this.updateCow(input);
     this.history.push(this.spoken);
-    console.log('this.history', this.history);
   };
 
   this.undo = function(){
     $log.debug('this.undo()');
-    console.log('this.history', this.history);
     this.history.pop();
-    this.spoken = this.history.pop() || ''; 
+    this.spoken = this.history.pop() || '';
   };
 }
